a1a3bdf02426f80e827590af2f9bd207aed35c9f
rfloweree <rubenloweree@gmail.com>
2020-08-20 13:03:03 -0600

    Added Factory producer.This will create factories for the different parser families.

---
+++ src/main/java/com/rfl/apps/runeconverterapi/parser/FactoryProducer.java
@@ -0,0 +1,15 @@
+package com.rfl.apps.runeconverterapi.parser;
+
+/**
+ * Object to create a Parser Factory for a specific parser family. Parser 
+ * families are based on language character representation or different 
+ * variations of the same alphabet used within the language.
+ * 
+ * E.g. Khuzdul, dwarvish language, uses the Cirth rune system, but there two
+ * rune systems, Erebor and Moria. Each is treated as a separate parser.
+ * 
+ * @author Ruben F. Loweree Jr.
+ */
+public class FactoryProducer {
+    
+}
