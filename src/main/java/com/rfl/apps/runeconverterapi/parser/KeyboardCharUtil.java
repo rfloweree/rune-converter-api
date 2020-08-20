2f065b83f89bde7ed64415b38457c6cd265be0c0
rfloweree <rubenloweree@gmail.com>
2020-08-20 11:58:02 -0600

    Added a Utility class that handles magic characters throughout the API. This class is for convenience and should not alter the functions of this API.

---
+++ src/main/java/com/rfl/apps/runeconverterapi/parser/KeyboardCharUtil.java
@@ -0,0 +1,100 @@
+package com.rfl.apps.runeconverterapi.parser;
+
+/**
+ * Utility wrapper to prevent magic characters throughout the patterns.
+ * 
+ * @author Ruben F. Loweree Jr.
+ */
+public class KeyboardCharUtil {
+    public static final char NUMBER_ONE = '1';
+    public static final char NUMBER_TWO = '2';
+    public static final char NUMBER_THREE = '3';
+    public static final char NUMBER_FOUR = '4';
+    public static final char NUMBER_FIVE = '5';
+    public static final char NUMBER_SIX = '6';
+    public static final char NUMBER_SEVEN = '7';
+    public static final char NUMBER_EIGHT = '8';
+    public static final char NUMBER_NINE = '9';
+    public static final char NUMBER_ZERO = '0';
+    
+    public static final char LOWERCASE_A = 'a';
+    public static final char LOWERCASE_B = 'b';
+    public static final char LOWERCASE_C = 'c';
+    public static final char LOWERCASE_D = 'd';
+    public static final char LOWERCASE_E = 'e';
+    public static final char LOWERCASE_F = 'f';
+    public static final char LOWERCASE_G = 'g';
+    public static final char LOWERCASE_H = 'h';
+    public static final char LOWERCASE_I = 'i';
+    public static final char LOWERCASE_J = 'j';
+    public static final char LOWERCASE_K = 'k';
+    public static final char LOWERCASE_L = 'l';
+    public static final char LOWERCASE_M = 'm';
+    public static final char LOWERCASE_N = 'n';
+    public static final char LOWERCASE_O = 'o';
+    public static final char LOWERCASE_P = 'p';
+    public static final char LOWERCASE_Q = 'q';
+    public static final char LOWERCASE_R = 'r';
+    public static final char LOWERCASE_S = 's';
+    public static final char LOWERCASE_T = 't';
+    public static final char LOWERCASE_U = 'u';
+    public static final char LOWERCASE_V = 'v';
+    public static final char LOWERCASE_W = 'w';
+    public static final char LOWERCASE_X = 'x';
+    public static final char LOWERCASE_Y = 'y';
+    public static final char LOWERCASE_Z = 'z';
+    
+    public static final char UPPERCASE_A = 'A';
+    public static final char UPPERCASE_B = 'B';
+    public static final char UPPERCASE_C = 'C';
+    public static final char UPPERCASE_D = 'D';
+    public static final char UPPERCASE_E = 'E';
+    public static final char UPPERCASE_F = 'F';
+    public static final char UPPERCASE_G = 'G';
+    public static final char UPPERCASE_H = 'H';
+    public static final char UPPERCASE_I = 'I';
+    public static final char UPPERCASE_J = 'J';
+    public static final char UPPERCASE_K = 'K';
+    public static final char UPPERCASE_L = 'L';
+    public static final char UPPERCASE_M = 'M';
+    public static final char UPPERCASE_N = 'N';
+    public static final char UPPERCASE_O = 'O';
+    public static final char UPPERCASE_P = 'P';
+    public static final char UPPERCASE_Q = 'Q';
+    public static final char UPPERCASE_R = 'R';
+    public static final char UPPERCASE_S = 'S';
+    public static final char UPPERCASE_T = 'T';
+    public static final char UPPERCASE_U = 'U';
+    public static final char UPPERCASE_V = 'V';
+    public static final char UPPERCASE_W = 'W';
+    public static final char UPPERCASE_X = 'X';
+    public static final char UPPERCASE_Y = 'Y';
+    public static final char UPPERCASE_Z = 'Z';
+    
+    public static final char COMMA = ',';
+    public static final char PERIOD = '.';
+    public static final char BACKSLASH = '/';
+    public static final char SEMICOLON = ';';
+    public static final char EXCLAMATION_MARK = '!';
+    public static final char AT_SYMBOL = '@';
+    public static final char HASHMARK = '#';
+    public static final char DOLLAR_SIGN = '$';
+    public static final char PERCENT_SIGN = '%';
+    public static final char AMPERSAND = '&';
+    public static final char ASTERISK = '*';
+    public static final char LEFT_PARENTHESES = '(';
+    public static final char RIGHT_PARANTHESES = ')';
+    public static final char UNDERSCORE = '_';
+    public static final char PLUS = '+';
+    public static final char VERTICAL_BAR = '|';
+    public static final char LEFT_BRACE = '{';
+    public static final char RIGHT_BRACE = '}';
+    public static final char RIGHT_BRACKET = ']';
+    public static final char EQUALS = '=';
+    public static final char FORWARDSLASH = '\\';
+    public static final char COLON = ':';
+    public static final char QUOTATION_MARK = '"';
+    public static final char LESS_THAN = '<';
+    public static final char GREATER_THAN = '>';
+    public static final char QUESTION_MARK = '?';
+}
