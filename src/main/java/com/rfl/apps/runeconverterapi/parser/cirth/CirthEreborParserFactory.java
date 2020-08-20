970179c843e5d1e5100dcf5c541b21d77285957d
rfloweree <rubenloweree@gmail.com>
2020-08-20 13:07:33 -0600

    Added Parser Factory for Cirth Erebor.

---
+++ src/main/java/com/rfl/apps/runeconverterapi/parser/cirth/CirthEreborParserFactory.java
@@ -0,0 +1,17 @@
+package com.rfl.apps.runeconverterapi.parser.cirth;
+
+import com.rfl.apps.runeconverterapi.parser.Parser;
+import com.rfl.apps.runeconverterapi.parser.ParserFactory;
+
+/**
+ * 
+ * @author Ruben F. Loweree Jr.
+ */
+public class CirthEreborParserFactory implements ParserFactory {
+
+    @Override
+    public Parser createParser(String parserType) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
