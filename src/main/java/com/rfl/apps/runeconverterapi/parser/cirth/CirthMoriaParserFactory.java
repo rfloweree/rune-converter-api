e26439801ffe8f025957ee3315bbf17da2262901
rfloweree <rubenloweree@gmail.com>
2020-08-20 14:38:26 -0600

    Added Parser Factory for Cirth Moria.

---
+++ src/main/java/com/rfl/apps/runeconverterapi/parser/cirth/CirthMoriaParserFactory.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rfl.apps.runeconverterapi.parser.cirth;
+
+import com.rfl.apps.runeconverterapi.parser.Parser;
+import com.rfl.apps.runeconverterapi.parser.ParserFactory;
+
+/**
+ *
+ * @author ruben
+ */
+public class CirthMoriaParserFactory implements ParserFactory {
+
+    @Override
+    public Parser createParser(String parserType) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
